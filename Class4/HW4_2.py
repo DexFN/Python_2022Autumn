1082d1e07dacef841dd4282e954fa1818beee1bc
DexFN <louie@chiang1.com>
2022-11-06 14:28:04 +0800

    Class 4

--- Class4/HW4_2.py
+++ Class4/HW4_2.py
@@ -7,25 +7,25 @@ root.title("Class4_HW2")
 birth=Entry(root,font=('Arial',18))
 Label(root,text="Enter you birth date: \n Input format is yyyy.mm.dd").pack()
 def clicked():
-    birth_1=birth.get().split(".")
-    year = today.year - int(birth_1[0])              # 用今天的年份，減去使用者的生日年份 ( 年份差 )
-    month = today.month - int(birth_1[1])
-    if month<0:
-        year = year - 1
-        month = 12 + month
+    birth1=birth.get().split(".")
+    Year = today.year - int(birth1[0])              
+    Month = today.month - int(birth1[1])
+    if Month<0:
+        Year = Year - 1
+        Month = 12 + Month
     day_list = [31,28,31,30,31,30,31,31,30,31,30,31]
-    day = today.day - int(birth_1[2])
+    day = today.day - int(birth1[2])
     if day<0:
-        month = month - 1
-    if month<0:
-        year = year - 1
-        month = 12 + month
-    day = day_list[month] + day
+        Month = Month - 1
+    if Month<0:
+        Year = Year - 1
+        Month = 12 + Month
+    day = day_list[Month] + day
 
-    Label(root,text=f"{year} years old, {month} months, {day} days.").pack()
+    Label(root,text=f"{Year} years old, {Month} months, {day} days.").pack(pady=20)
 
 
-Button2=Button(root,text="enter",command=clicked)
+Button2=Button(root,text="Enter!",command=clicked)
 
 birth.pack()
 Button2.pack()
